5df0e8f155ba492f72c6015f6934a290d5be5236
Clement Ramondou <cl.ramondou@gmail.com>
2019-03-31 20:35:54 +0200

    fix traduction path

--- src/app/shared/services/translate.service.ts
+++ src/app/shared/services/translate.service.ts
@@ -13,7 +13,7 @@ export class TranslateService {
 
   use(lang: string): Promise<{}> {
     return new Promise<{}>((resolve, reject) => {
-      this.http.get<{}>('../../assets/i18n/' + `${lang || 'fr'}.json`).subscribe(
+      this.http.get<{}>('../../../assets/i18n/' + `${lang || 'fr'}.json`).subscribe(
         translation => {
           this.data = Object.assign({}, translation || {});
           resolve(this.data);
